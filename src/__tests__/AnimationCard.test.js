0d4e95a130cc770aa43b2c29eb84df2a47cd42fd
kayjee <kagiso.killagram@gmail.com>
2023-07-15 13:49:26 +0200

    updated animation test

---
+++ src/__tests__/AnimationCard.test.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import AnimeCard from '../components/AnimationCard';
+
+describe('AnimationCard component', () => {
+  it('matches the snapshot', () => {
+    // Render the AnimeCard component with the Redux store provided by the Provider
+    const { container } = render(w
+      <Provider store={store}>
+        <AnimeCard />
+      </Provider>,
+    );
+
+    // Compare the rendered component with the snapshot
+    expect(container).toMatchSnapshot();
+  });
+});
